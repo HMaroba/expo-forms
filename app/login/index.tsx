676e72125158050e11f51a3a3a575304a3c16e03
Coffee58511867 <marobahlalele910@gmail.com>
2024-06-21 20:27:30 +0200

    Fixed app

---
+++ app/login/index.tsx
@@ -0,0 +1,28 @@
+import { styles } from "@/styles/styles";
+import React from "react";
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Register() {
+  const insets = useSafeAreaInsets();
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <View style={{ paddingTop: insets.top }}>
+        <Text>Email Address</Text>
+        <TextInput style={styles.input} placeholder="Email" />
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaProvider>
+  );
+}
