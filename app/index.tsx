079ec114299dc80f3e08622a28766afe5ff87cad
Coffee58511867 <marobahlalele910@gmail.com>
2024-06-21 20:28:31 +0200

    Fixed app

--- app/index.tsx
+++ app/index.tsx
@@ -8,11 +8,15 @@ export default function HomePage() {
   const register = () => {
     router.push('/register');
   }
+
+  const login = () => {
+    router.push('/login');
+  }
   return (
     <View style={styles.container}>
       <Text>Welcome To Lebelo</Text>
       <Link href="/register">Register now </Link>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={()=> login()}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={()=> register()}>
