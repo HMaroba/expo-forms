087901d2311fb6b5bc4e23619e5633aa42d34d2b
Coffee58511867 <marobahlalele910@gmail.com>
2024-06-24 17:17:21 +0200

    Created balance component

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 import Feather from "@expo/vector-icons/Feather";
+import { router } from "expo-router";
 import React from "react";
 import { View, Text } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -7,13 +8,17 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 type IconProps = {
   iconName?: any;
   title: string;
+  link?: any;
 };
 
-export default function CardInfo({ iconName, title }: IconProps) {
-
-    const IconComponent = Ionicons;
+export default function CardInfo({ iconName, title, link }: IconProps) {
+  const IconComponent = Ionicons;
+  const login = () => {
+    router.push(link);
+  };
   return (
     <TouchableOpacity
+      onPress={() => login()}
       style={{
         backgroundColor: "green",
         width: "25%",
@@ -24,7 +29,9 @@ export default function CardInfo({ iconName, title }: IconProps) {
       }}
     >
       <IconComponent name={iconName} size={30} color="white" />
-      <Text style={{ color: "white", fontSize: 14 , paddingTop: 2}}>{title}</Text>
+      <Text style={{ color: "white", fontSize: 14, paddingTop: 2 }}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
