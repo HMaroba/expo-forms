e10d85cb343561306e0d61b08223588336ace523
Coffee58511867 <marobahlalele910@gmail.com>
2024-06-24 17:12:23 +0200

    Created balance component

--- app/screens/dashboard/transactions/index.tsx
+++ app/screens/dashboard/transactions/index.tsx
@@ -1,7 +1,13 @@
 import { styles } from "@/styles/styles";
 import { Link } from "expo-router";
 import React from "react";
-import { View, TextInput, Text, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -75,7 +81,7 @@ export default function Register() {
               height: 70,
               width: "45%",
               borderWidth: 2,
-              borderRadius: 10,
+              borderRadius: 5,
               borderColor: "#4FA64F",
               backgroundColor: "#4FA64F",
             }}
@@ -83,7 +89,7 @@ export default function Register() {
             <Text style={{ fontSize: 24, fontWeight: 400, color: "white" }}>
               LSL 5600.00
             </Text>
-            <Text style={{color: 'white'}}>Received</Text>
+            <Text style={{ color: "white" }}>Received</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{
@@ -92,7 +98,7 @@ export default function Register() {
               height: 70,
               width: "45%",
               borderWidth: 2,
-              borderRadius: 10,
+              borderRadius: 5,
               borderColor: "#4FA64F",
               backgroundColor: "#4FA64F",
             }}
@@ -100,7 +106,7 @@ export default function Register() {
             <Text style={{ fontSize: 24, fontWeight: 400, color: "white" }}>
               LSL 600.00
             </Text>
-            <Text style={{color: 'white'}}>Paid</Text>
+            <Text style={{ color: "white" }}>Paid</Text>
           </TouchableOpacity>
         </View>
 
@@ -108,31 +114,39 @@ export default function Register() {
           <Text style={{ fontSize: 20, paddingTop: 10 }}>
             Recent Transactions
           </Text>
-          <ScrollView style={{marginBottom: 100, height: 450}}>
-          <View>
-            {TRansactions.map((item: any, index: number) => (
-              <View
-                key={index}
-                style={{
-                  borderWidth: 2,
-                  borderColor: "green",
-                  borderRadius: 5,
-                  marginTop: 10,
-                  padding: 5,
-                  flexDirection: 'row'
-                }}
-              >
-                <View>
-                  <Text style={{ fontSize: 18 }}>{item.name}</Text>
-                  <Text style={{ fontSize: 18 }}>{item.type}</Text>
+          <ScrollView style={{ marginBottom: 100, height: 450 }}>
+            <View>
+              {TRansactions.map((item: any, index: number) => (
+                <View key={index}>
+                <View
+                  
+                  style={{
+                    // borderWidth: 2,
+                    // borderColor: "green",
+                    // borderRadius: 5,
+                    marginTop: 10,
+                    padding: 5,
+                    flexDirection: "row",
+                  }}
+                >
+                  <View>
+                    <Text style={{ fontSize: 18 }}>{item.name}</Text>
+                    <Text style={{ fontSize: 14, color: 'gray' }}>{item.type}</Text>
+                  </View>
+                  <View style={{ marginLeft: "auto" }}>
+                    <Text style={{ fontSize: 18, fontWeight: 600 }}>
+                      LSL {item.amount}
+                    </Text>
+                  </View>
+                  
                 </View>
-                <View style={{marginLeft: 'auto'}}>
-                  <Text style={{ fontSize: 18, fontWeight: 600 }}>LSL {item.amount}</Text>
-               
+                <View style={{flex: 1, height: 2, backgroundColor: 'gray'}} />
                 </View>
-              </View>
-            ))}
-          </View>
+                
+              ))}
+         
+            
+            </View>
           </ScrollView>
         </View>
       </View>
